acd825174bd2b75b84c679ac2761f42d41a3c232
mPasGamer <66804673+mPasGamer@users.noreply.github.com>
2023-12-30 11:24:53 -0500

    Updated page-markp-02.php
    formatted dates
    prepped modal query

--- wp-content/themes/sparkling/page-markp-02.php
+++ wp-content/themes/sparkling/page-markp-02.php
@@ -36,10 +36,16 @@ get_header(); ?>
 
 		<?php
 			global $wpdb;
-			$row = $wpdb->get_results("select ct.CompetitionType, c.Duration, c.StartDate, c.EndDate, c.Placed, c.WeekNmbr 
+			$row = $wpdb->get_results("select c.CompetitionId, ct.CompetitionType, c.Duration, 
+				c.StartDate, c.EndDate,	c.Placed, c.WeekNmbr 
 				from wpsq_jj_competitions c inner join wpsq_jj_competition_types ct
 				on c.CompetitionTypeId = ct.CompetitionTypeId
 				Order By c.StartDate DESC, ct.CompetitionType");
+			$scores = $wpdb->get_results("SELECT PlayerName, Score
+				FROM wpsq_jj_player_scores ps inner join wpsq_jj_players p
+				on ps.PlayerId = p.PlayerId
+				Where CompetitionId = 58
+				Order By Score DESC");
 		?>
 			<table class="table table-striped">
 				<thead>
@@ -59,38 +65,37 @@ get_header(); ?>
 							<tr>
 <!-- https://www.w3schools.com/PHP/func_date_date_format.asp -->
 								<td class="center-text">
-									<button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+ 									<button type="button" comp-id="<?php echo $comp->CompetitionId; ?>" data-bs-toggle="modal" data-bs-target="#exampleModal">
 										<i class="bi bi-plus-circle"></i>
-									</button>
+									</button>									
 								</td>
 								<td><?php echo $comp->CompetitionType; ?></td>
 								<td class="center-text"><?php echo $comp->Duration; ?></td>
-								<td class="center-text"><?php echo $comp->StartDate; ?></td>
-								<td class="center-text"><?php echo $comp->EndDate; ?></td>
+								<td class="center-text"><?php echo date('m/d/y h:i a', strtotime($comp->StartDate)); ?></td>
+								<td class="center-text"><?php echo date('m/d/y h:i a', strtotime($comp->EndDate)); ?></td>
 								<td class="center-text"><?php echo $comp->Placed; ?></td>
 								<td class="center-text"><?php echo $comp->WeekNmbr; ?></td>
-							</tr>
-					<?php } ?>
-
-				</tbody><!-- #tbody -->
 <!-- Modal -->
-					<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-						<div class="modal-dialog">
-							<div class="modal-content">
-								<div class="modal-header">
-									<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-									<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-								</div>
-								<div class="modal-body">
-									...
-								</div>
-								<div class="modal-footer">
-									<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-									<button type="button" class="btn btn-primary">Save changes</button>
-								</div>
-							</div>
-						</div>
-					</div>			</table><!-- #table -->
+								<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+									<div class="modal-dialog">
+										<div class="modal-content">
+											<div class="modal-header">
+												<h5 class="modal-title" id="exampleModalLabel">Player Scores</h5>
+												<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+											</div>
+											<div class="modal-body">
+												...
+											</div>
+											<div class="modal-footer">
+												<button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+											</div>
+										</div>
+									</div>
+								</div><!-- Modal -->								
+							</tr>						
+					<?php } ?>	
+				</tbody><!-- #tbody -->
+			</table><!-- #table -->
 	</main><!-- #main -->
 
   </div><!-- #primary -->
@@ -98,3 +103,4 @@ get_header(); ?>
 </div><!-- close .main-content-inner -->
 
 <?php get_footer(); ?>
+
